d3050be9cb0cbb8d5821fdaa42e35d7d223fd1fd
Harsh gour <gourharsh8909@gmail.com>
2025-12-14 22:05:16 +0530

    first commit

---
+++ src/test/java/com/example/Decentralized_Chat_Application/DecentralizedChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Decentralized_Chat_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentralizedChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
